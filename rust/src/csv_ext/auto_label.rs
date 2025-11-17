a9ee6e845d51c073ab18115b420dae4f65327ff2
Shems <shems.petremand.etu@a12p7.fil.univ-lille.fr>
2025-11-17 16:25:53 +0100

    map_record_to_vec & adapted stats computation with it

--- rust/src/csv_ext/auto_label.rs
+++ rust/src/csv_ext/auto_label.rs
@@ -71,38 +71,79 @@ pub fn sniff_labels(path: String) -> Result<bool, Box<dyn std::error::Error>> {
     Ok(true)
 }
 
-pub fn get_stats(records: &mut ByteRecordsIter<File>, max_obs: Option<usize>) -> Result<Vec<Vec<usize>>, Box<dyn std::error::Error>> {
-    // compute statistics of messages
+
+pub fn map_records_to_vec(records: &mut ByteRecordsIter<File>, max_obs: Option<usize>) -> Result<Vec<Vec<Vec<u8>>>, Box<dyn std::error::Error>> {
+    // Maps the byte records as a 2 dimensionnal array to ease exploration by columns
     // Vector of columns, that is, vector of vectors
     //      each sub vector is a column
     
     // Helper function to avoid doing a if stats.empty while iterating on records
     //          While also avoiding code dupplication !
-    fn get_record_stats(record: &mut ByteRecord, idx: usize, stats: &mut Vec<Vec<usize>>) {
+    fn map_record_in_vec(record: &mut ByteRecord, idx: usize, vec: &mut Vec<Vec<Vec<u8>>>) {
         record.iter().for_each(|bytes| {
-            let (text, _) = detect_and_decode(bytes);
-            stats[idx].push(text.len());
+            vec[idx].push(bytes.to_vec());
         });
     }
 
     let mut record = records.next().unwrap()?;
-    let mut stats: Vec<Vec<usize>> = vec![vec![]; record.len()];
-    get_record_stats(&mut record, 0, &mut stats);
+    let mut vectored: Vec<Vec<Vec<u8>>> = vec![vec![]; record.len()];
+    map_record_in_vec(&mut record, 0, &mut vectored);
 
     let max_obs = max_obs.unwrap_or(usize::MAX);
 
     for (i, record) in records.enumerate() {
         let real_idx = i+1; // i + 1 is eww I WANT enumerate(n) SO BADLY https://github.com/rust-itertools/itertools/issues/815
         
-        if max_obs <= i {
-            break;
-        }
+        if max_obs <= i {break}
 
         let mut record = record?;
-        get_record_stats(&mut record, real_idx, &mut stats);
+        map_record_in_vec(&mut record, real_idx, &mut vectored);
     }
-    
-    Ok(stats)
+
+    Ok(vectored)
+
+}
+
+
+/* Could be interesting not to clone, let's see later how i can avoid the ownership error
+the error is that vectored references bytes, which is owned by record, which is a local variable
+records.next() produces an owned variable, not a reference to a record.
+
+How can we get a reference instead ?
+
+pub fn map_records_to_vec<'a>(records: &'a mut ByteRecordsIter<File>, max_obs: Option<usize>) -> Result<Vec<Vec<&'a [u8]>>, Box<dyn std::error::Error>> {
+    fn map_record_in_vec<'a>(record: &'a mut ByteRecord, idx: usize, vec: &mut Vec<Vec<&'a [u8]>>) {
+        record.iter().for_each(|bytes| {
+            vec[idx].push(bytes);
+        });
+    }
+
+    let mut record = records.by_ref().next().unwrap()?;
+    let mut vectored: Vec<Vec<&[u8]>> = vec![vec![]; record.len()];
+    map_record_in_vec(&mut record, 0, &mut vectored);
+
+
+    Ok(vectored)
+
+}
+*/ 
+
+pub fn map_to_string_size(veced_records: &[Vec<Vec<u8>>]) -> Vec<Vec<usize>> {
+    // Maps a 2D vector of bytes to the size of the corresponding string (according to its guessed encoding)
+    // Vector of columns, that is, vector of vectors
+    //      each sub vector is a column
+    veced_records
+        .iter()
+        .map(|record| {
+            record
+                .iter()
+                .map(|bytes| {
+                    let (text, _) = detect_and_decode(bytes);
+                    text.len()
+                })
+                .collect()
+        })
+        .collect()
 }
 
 // Tries to infer which column contains the rating.
